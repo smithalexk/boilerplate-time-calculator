b258898c9e4be9b986da8cb038b144550e8ecb72
Alex Smith <alex.smith@perspectum.com>
2021-05-16 22:09:25 +0100

    Built add_time for start, duration args -Still need to add the day optional arg

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,33 @@
-def add_time(start, duration):
+import re
 
 
+def add_time(start, duration, day=None):
+    hours = [int(re.search("([0-9]+):", x).groups()[0]) for x in [start, duration]]
+    minutes = [int(re.search(":([0-9]+)", x).groups()[0]) for x in [start, duration]]
 
+    if "PM" in start:
+        hours[0] = hours[0] + 12
 
+    total_hours = sum(hours)
+    total_minutes = sum(minutes)
+
+    total_hours += total_minutes // 60
+
+    days = total_hours // 24
+    
+    am_pm_flag = "AM" if total_hours % 24 < 12 else "PM"
+
+    o_hours = total_hours % 24
+    if o_hours > 12:
+        o_hours -= 12
+    
+    o_minutes = total_minutes % 60
+
+    new_time = f"{o_hours}:{o_minutes:02d} {am_pm_flag}"
+
+    if days == 1:
+        new_time = " ".join([new_time, "(next day)"])
+    elif days > 1:
+        new_time = " ".join([new_time,f"({days} days later)"])
 
     return new_time
