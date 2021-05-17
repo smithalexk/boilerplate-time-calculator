06ca95777f56fab1411f13116d19ccfdfa2c73a4
Alex Smith <alex.smith@perspectum.com>
2021-05-17 20:55:06 +0100

    Added Optional Day argument - Fixed bug for 12 AM

--- time_calculator.py
+++ time_calculator.py
@@ -1,10 +1,21 @@
 import re
 
 
-def add_time(start, duration, day=None):
+weekdays = {
+    1: "sunday",
+    2: "monday",
+    3: "tuesday",
+    4: "wednesday",
+    5: "thursday",
+    6: "friday",
+    7: "saturday",
+}
+
+
+def add_time(start: str, duration: str, day: str = None) -> str:
     hours = [int(re.search("([0-9]+):", x).groups()[0]) for x in [start, duration]]
     minutes = [int(re.search(":([0-9]+)", x).groups()[0]) for x in [start, duration]]
-
+    
     if "PM" in start:
         hours[0] = hours[0] + 12
 
@@ -14,20 +25,32 @@ def add_time(start, duration, day=None):
     total_hours += total_minutes // 60
 
     days = total_hours // 24
-    
+
+    if day:
+        day_num = next((num for num, wd in weekdays.items() if wd == day.lower()), None)
+        
+        day_num += days
+
+
     am_pm_flag = "AM" if total_hours % 24 < 12 else "PM"
 
     o_hours = total_hours % 24
     if o_hours > 12:
         o_hours -= 12
-    
+    elif o_hours == 0:
+        o_hours = 12
+
     o_minutes = total_minutes % 60
 
     new_time = f"{o_hours}:{o_minutes:02d} {am_pm_flag}"
 
+    if day:
+        new_time = ", ".join([new_time, weekdays[day_num % 7].capitalize()])
+
     if days == 1:
         new_time = " ".join([new_time, "(next day)"])
     elif days > 1:
-        new_time = " ".join([new_time,f"({days} days later)"])
+        new_time = " ".join([new_time, f"({days} days later)"])
+
 
     return new_time
